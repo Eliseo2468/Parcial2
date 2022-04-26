938146de441445e01243ee18b846ef8f59974031
Eliseo2468 <100702315+Eliseo2468@users.noreply.github.com>
2022-04-26 08:27:45 -0600

    Add files via upload

---
+++ Config.js
@@ -0,0 +1,18 @@
+
+function opcion(){
+    let selectedValue = document.getElementById("seleccion").value;
+    
+    if(selectedValue == 1){
+        var Res=prompt("ingrese el id:");
+        var Res2=prompt("Donde quiere insertar el elemento?:");
+
+    }
+    if(selectedValue == 2){
+        var Res=prompt("ingrese el id:");
+        var Res2=prompt("Donde quiere insertar el elemento?:");
+    }
+    if(selectedValue == 3){
+        var Res=prompt("ingrese el id:");
+        var Res2=prompt("Donde quiere insertar el elemento?:");
+    }
+  }
